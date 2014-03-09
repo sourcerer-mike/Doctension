d112f3f4715442fc093a451a798018524eafaea3
Mike Pretzlaw <pretzlaw@gmail.com>
2014-03-09 13:17:45 +0100

    divides in sections and translatable

--- src/app/code/community/Acme/Doc/Model/Parser/Module.php
+++ src/app/code/community/Acme/Doc/Model/Parser/Module.php
@@ -10,29 +10,46 @@ class Acme_Doc_Model_Parser_Module implements Acme_Doc_Model_Parser_Interface
      */
     public function parse($data, Acme_Doc_Model_Output_Interface $output)
     {
+
         $output->addHeading($data->getName());
 
+        $this->mainSection($data, $output);
+        $this->configSection($data, $output);
+
+        $output->addLine();
+        $output->addLine();
+    }
+
+    /**
+     * @param                                 $data
+     * @param Acme_Doc_Model_Output_Interface $output
+     */
+    private function configSection($data, Acme_Doc_Model_Output_Interface $output)
+    {
+        $configParser = Mage::getModel('acme_doc/parser_config');
+        $configParser->parse($data->getConfig(), $output->getSub());
+    }
+
+    /**
+     * @param                                 $data
+     * @param Acme_Doc_Model_Output_Interface $output
+     */
+    private function mainSection($data, Acme_Doc_Model_Output_Interface $output)
+    {
+        $helper  = Mage::helper('acme_doc');
         $version = $data->getVersion();
         if (!$version)
         {
-            $version = sprintf('<i>%s</i>', Mage::helper('acme_doc')->__('unknown'));
+            $version = sprintf('<i>%s</i>', $helper->__('unknown'));
         }
 
         $output->addItemization(
             array(
-                'Active: ' . $data->getActive(),
-                'Version: ' . $version,
-                'Code Pool: ' . $data->getCodePool(),
-                'Directory: ' . $data->getDirectory(),
+                $helper->__('Active: %s', $data->getActive()),
+                $helper->__('Version: %s', $version),
+                $helper->__('Code Pool: %s', $data->getCodePool()),
+                $helper->__('Directory: %s', $data->getDirectory()),
             )
         );
-
-        // config
-
-        $configParser = Mage::getModel('acme_doc/parser_config');
-        $configParser->parse($data->getConfig(), $output->getSub());
-
-        $output->addLine();
-        $output->addLine();
     }
 }
